5fd2143fa7916aba74ba501a2f89c51a989da75e
질문shinha < shlee2318@naver.com>
2022-02-15 23:05:53 +0900

    django course 14 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -132,9 +132,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = {
+STATICFILES_DIRS = (
     BASE_DIR / "static",
-}
+)
 
 
 # Default primary key field type
